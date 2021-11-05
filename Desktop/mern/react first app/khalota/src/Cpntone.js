99db501a0fafde60fc15e7d64d787e605a144a4c
walid chebbab <walidchebbab2001@gmail.com>
2021-11-05 11:40:13 +0100

    checkpoint

---
+++ Desktop/mern/react first app/khalota/src/Cpntone.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cpntone = (props) => {
+    return (
+        <div>
+            {props.t}
+            {props.b}
+        </div>
+    )
+}
+
+export default Cpntone
---
+++ Desktop/mern/react first app/khalota/src/components/test/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <React.Fragment>
+            test
+        </React.Fragment>
+    )
+}
+
+export default Test
